472b339f69e014dbb736a77ad5b442fb79807869
Darya Dyachkova <darya.dyachkova@minerva.kgi.edu>
2020-03-26 21:57:36 +0800

    Update ResNetVAE_Modules.py

--- ResNetVAE_Modules.py
+++ ResNetVAE_Modules.py
@@ -1,24 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import os 
 import sys
 import numpy as np
 import glob
 import glob
 
-
-# In[2]:
-
-
 wdir = r'C:\Users\Darya\Documents\GitHub\Autoencoders'
 os.chdir(wdir)
-
-
-# In[3]:
 
 
 import torch
@@ -49,15 +39,11 @@ import functools
 from time import time 
 
 
-# In[ ]:
-
-
+# the program stops once the model predicts zero-tensors.
 class ZeroTensorPrediction(Exception): pass
 
 
-# In[6]:
-
-
+# this is for plotting the images. 
 def plot_img(X):
     rcParams['figure.figsize'] = 3,3
     try:
@@ -70,9 +56,7 @@ def plot_img(X):
         return None 
 
 
-# In[7]:
-
-
+# load the Parquet Dataset and perform pre-processing. 
 class ParquetDataset(Dataset):
     def __init__(self, filename):
         self.parquet = pq.ParquetFile(filename)
@@ -90,10 +74,7 @@ class ParquetDataset(Dataset):
     def __len__(self):
         return self.parquet.num_row_groups
 
-
-# In[12]:
-
-
+#initialize the resnet and the optimizer, given the number of channels and resblocks. 
 def initialize(resblocks, n_channels):
     resnet = networks.AutoEncoder(n_channels, resblocks, [16, 32, 64, 128])
     optimizer = optim.Adam(resnet.parameters(), lr=5.e-4)
@@ -101,9 +82,7 @@ def initialize(resblocks, n_channels):
     return resnet, optimizer
 
 
-# In[1]:
-
-
+#initialize the train and validation loaders. 
 def train_val_loader(datasets, train_cut, batch_size, random_sampler=True):
     dset = ConcatDataset([ParquetDataset(dataset) for dataset in datasets])
     idxs = np.random.permutation(len(dset))
@@ -119,17 +98,17 @@ def train_val_loader(datasets, train_cut, batch_size, random_sampler=True):
     return train_loader, val_loader
 
 
-# In[16]:
-
-
+# calculate the loss function: sum over the channels and scale up by the factor of 1000. 
 def calc_loss(n_channels, X, Xreco):
-    mse = lambda X, Xreco, n: F.mse_loss(Xreco[:, n, :, :], X[:, n, :, :])
-    return 1000*sum(map(partial(mse, X, Xreco), range(n_channels)))  
+    sc_fact_arr = [13, 5, 5.5]
+    mse_total = lambda X, Xreco, n: F.mse_loss(Xreco[:, n, :, :], X[:, n, :, :])
+    mse_ind = lambda X, Xreco: F.mse_loss(Xreco, X)
+    if n_channels == 3:
+        return sum(map(partial(mse_total, X, Xreco), range(n_channels))) 
+    else: 
+        return mse_ind(X, Xreco) 
 
-
-# In[4]:
-
-
+# at each monitor step, print the original and reconstructed image, together with the loss. 
 def print_intermadiate_results(i, X, X_ind, Xreco, name, epoch, loss, len_train_loader, imgs, loss_map = False):
     if X_ind < 3:
         img = X[0].cpu().numpy().reshape(85, 85)
@@ -154,11 +133,11 @@ def print_intermadiate_results(i, X, X_ind, Xreco, name, epoch, loss, len_train_
     print('%d: (%d/%d) Train loss:%f, Emax: %f, Erecomax: %f'%(epoch, i, len_train_loader, loss.item(), img.max(), img_reco.max()))        
 
 
-# In[11]:
-
+# calculate the earth mover distance. 
 def calc_emd(img1, img2): 
     return emd(img1, img2, R=1.0, norm=False, beta=1.0, measure='euclidean', coords='hadronic', return_flow=False, gdim=None, mask=False, n_iter_max=100000, periodic_phi=False, phi_col=2, empty_policy='error')
 
+# calculate the image in the batch that is the closest to the average image. 
 def calc_emd_reference(X, ind):
     sample = X[:, ind, :, :].reshape(X.shape[0], X.shape[2], X.shape[3])
     avg = np.average(sample,axis=0)
@@ -167,139 +146,104 @@ def calc_emd_reference(X, ind):
         emd_arr = np.append(emd_arr, calc_emd(avg, sample[i]))
     return sample[emd_arr.argmin()]
 
+# calculate emd between the reference image and each image in the batch. 
 def calc_emd_vals(X, X_ind):
     reference = calc_emd_reference(X, X_ind)
     X_sample = X.cpu().numpy()[:, X_ind, :, :].reshape(X.shape[0], X.shape[2], X.shape[3])
     emd_vals = list(map(functools.partial(calc_emd, reference), X_sample))
     return emd_vals
 
+# loads the original data. 
+def get_X(data, X_ind):
+    if X_ind != 3:
+        X = data['X_jets'][:, X_ind, :, :].reshape(data['X_jets'][:, X_ind, :, :].shape[0],  1, 85, 85).cuda()
+    else: 
+        X = data['X_jets'].cuda()
+    return X
+
+# evaluation code, also calculates emd and losses. 
 def do_eval(resnet, val_loader, epoch, X_ind, n_channels, idx=0):
-    loss_ = []
-    emd_arr = []
-    ind_loss = []
+    loss_, emd_vals_ind_loss = [], []
     a = time()
     monitor_step = 10
     for i, data in enumerate(val_loader):
         if i % monitor_step == 0:
             print (i)
-        if X_ind != 3:
-            X = data['X_jets'][:, X_ind, :, :].reshape(data['X_jets'][:, X_ind, :, :].shape[0],  1, 85, 85).cuda()
-            emd_vals = calc_emd_vals(X, X_ind)
-        else: 
-            X = data['X_jets'].cuda()
-            emd_vals = []
-            for i in range(3):
-                emd_vals.append(calc_emd_vals(X, i))
+        X = get_X(data, X_ind)
         Xreco = resnet(X)
         if X_ind != 3:
-            ind_loss = list(map(lambda x, xreco: np.sum((x - xreco) ** 2)/float(x.shape[0] * x.shape[1]), X.cpu().numpy()[:, X_ind, :, :], Xreco.detach().cpu().numpy()[:, X_ind, :, :]))
-        else: 
-            for i in range(3): 
-                ind_loss.append(list(map(lambda x, xreco: np.sum((x - xreco) ** 2)/float(x.shape[0] * x.shape[1]), X.cpu().numpy()[:, i, :, :], Xreco.detach().cpu().numpy()[:, i, :, :])))
-        X = X[...,1:-1,:]
-        Xreco = Xreco[...,1:-1,:]
-        losses = calc_loss(n_channels, X, Xreco)
-        loss_.append(losses.tolist())
-
-        
-    #now = time.time() - now
+            emd_vals_ind_loss = eval_losses(X.cpu().numpy()[:, X_ind, :, :], Xreco.detach().cpu().numpy()[:, X_ind, :, :], X_ind)
+        if X_ind == 3: 
+            emd_vals_ind_loss = list(map(partial(eval_losses, X.cpu().numpy()[:, X_ind-1, :, :], Xreco.detach().cpu().numpy()[:, X_ind-1, :, :]), range(0, X_ind)))
+        X, Xreco = X[...,1:-1,:], Xreco[...,1:-1,:]
+        loss_.append(calc_loss(n_channels, X, Xreco).tolist())
     loss_ = np.array(loss_)  
+    emd_vals, ind_loss = zip(*emd_vals_ind_loss)
     s = '%d: Val loss:%f, MAE: %f, N samples: %d in %f min'%(epoch, loss_.mean(), (np.sqrt(loss_)).mean(), len(loss_), (time() -a)/60.)
     print(s)
     return loss_, emd_vals, ind_loss
 
 
-# In[17]:
-
-
+# function for saving
+def save(add_dir, obj, n, expt_name, epoch, resnet, optimizer, new_model, name, ind):
+    filename = n + name + '_' + ind + '_epoch_' + str(epoch) + '.pkl'
+    directory = wdir + '\%s\%s'%(add_dir, expt_name) + '\\' + filename
+    if add_dir == 'MODELS':
+        obj = {'model': resnet.state_dict(), 'optim': optimizer.state_dict()}
+    torch.save(obj, directory)
+     
+    
+# save the model and metrics
 def save_model(wdir, expt_name, epoch, resnet, optimizer, metrics1, metrics2, metrics3, new_model, name):
+    metrics = [metrics1, metrics2, metrics3]
+    names_list = ['ResNet_VAE_model_', 'ResNet_VAE_loss_', 'ResNet_VAE_emd_vals_', 'ResNet_VAE_ind_loss_']
     wdir_models = wdir + '\%s\%s'%('MODELS', expt_name)
-    wdir_metrics1 = wdir + '\%s\%s'%('METRICS', expt_name)
-    wdir_metrics2 = wdir + '\%s\%s'%('METRICS', expt_name)
-    wdir_metrics3 = wdir + '\%s\%s'%('METRICS', expt_name)
-    #file_subs = [int(f.split('_')[3].split('.')[0]) for f in listdir(wdir_models) if (isfile(join(wdir_models, f))) and (f.split('.')[1] == 'pkl')]
-    file_subs = [f.split('_')[4] for f in listdir(wdir_models) if (isfile(join(wdir_models, f))) and (f.split('.')[1] == 'pkl') and (len(f.split('_')) > 6)]      
-    if new_model: 
+    file_subs = [int(f.split('_')[4]) for f in listdir(wdir_models) if (isfile(join(wdir_models, f))) and (f.split('.')[1] == 'pkl') and (len(f.split('_')) > 6)]      
+    if len(file_subs) == 0:
+        ind = str(1)
+    elif new_model: 
         ind = str(max(file_subs) + 1)
     else: 
         ind = str(max(file_subs))
-    ind = str(max(file_subs))
-    try: 
-        filename_models = 'ResNet_VAE_model_' + name + '_' + ind + '_epoch_' + str(epoch) + '.pkl'
-        filename_metrics1 = 'ResNet_VAE_loss_' + name + '_' +  ind + '_epoch_' + str(epoch) + '.pkl'
-        filename_metrics2 = 'ResNet_VAE_emd_vals_' + name + '_' +  ind + '_epoch_' + str(epoch) + '.pkl'
-        filename_metrics3 = 'ResNet_VAE_ind_loss_' + name + '_' +  ind + '_epoch_' + str(epoch) + '.pkl'
+    for n in names_list: 
+        if n == 'ResNet_VAE_model_': 
+            add_dir = 'MODELS'
+            obj = None
+        else: 
+            add_dir = 'METRICS'
+            obj = metrics[names_list.index(n) - 1]
+        save(add_dir, obj, n, expt_name, epoch, resnet, optimizer, new_model, name, ind)
 
-    except: 
-        filename_models = 'ResNet_VAE_model_' + name + '_' + str(1) + '_epoch_' + str(epoch) + '.pkl'
-        filename_metrics1 = 'ResNet_VAE_loss_' + name + '_' + str(1) + '_epoch_' + str(epoch) + '.pkl'
-        filename_metrics2 = 'ResNet_VAE_emd_vals_' + name + '_' + str(1) + '_epoch_' + str(epoch) + '.pkl'
-        filename_metrics3 = 'ResNet_VAE_ind_loss_' + name + '_' + str(1) + '_epoch_' + str(epoch) + '.pkl'
-
-    wdir_models += '\%s'%(filename_models)
-    wdir_metrics1 += '\%s'%(filename_metrics1)
-    wdir_metrics2 += '\%s'%(filename_metrics2)
-    wdir_metrics3 += '\%s'%(filename_metrics3)
-
-    model_dict = {'model': resnet.state_dict(), 'optim': optimizer.state_dict()}
-    torch.save(model_dict, wdir_models)
-    torch.save(metrics1, wdir_metrics1)
-    torch.save(metrics2, wdir_metrics2)
-    torch.save(metrics3, wdir_metrics3)
-
-
-
-
-# In[2]:
-
+        
+# actual training. First, load the resnet and optimizer, get the original data, get the reconstructed data, calculate the loss,
+# perform backproparagion, print out the results on the monitor step.
 
 def train(train_loader, val_loader, resblocks, n_epochs, name, batch_size, wdir, expt_name, imgs=False):
     name_mapping = {'Tracks': 0, 'ECAL': 1, 'HCAL': 2, 'ECAL HCAL Tracks': 3}
     channels_mapping = {'Tracks': 1, 'ECAL': 1, 'HCAL': 1,  'ECAL HCAL Tracks': 3}
-    X_ind = name_mapping[name]
-    n_channels = channels_mapping[name]
+    X_ind, n_channels, epochs, monitor_step = name_mapping[name], channels_mapping[name], n_epochs, 10
     resnet, optimizer = initialize(resblocks, n_channels)
-    resnet.cuda()
-    epochs = n_epochs
-    monitor_step = 10
     resnet.train()
-    metrics = []
     a = time()
     for e in range(epochs):
         epoch = e+1
-        s = '>> Epoch %d <<<<<<<<'%(epoch)
-        print(s)
-
-        print(">> Training <<<<<<<<")
-        #now = time.time()
+        print('>>>>>>>Epoch %d'%(epoch))
+        print(">>>>>>> Training")
         for i, data in enumerate(train_loader):
-            if X_ind != 3:
-                X = data['X_jets'][:, X_ind, :, :].reshape(data['X_jets'][:, X_ind, :, :].shape[0], 1, 85, 85).cuda()
-            else: 
-                X = data['X_jets'].cuda()
-            optimizer.zero_grad()
+            X = get_X(data, X_ind)
             Xreco = resnet(X)
+            optimizer.zero_grad()
             loss = calc_loss(n_channels, X, Xreco)
             loss.backward()
             optimizer.step()
             if i % monitor_step == 0:
-                #if i!= 0:
                 try:
                     print_intermadiate_results(i, X, X_ind, Xreco, name, epoch, loss, len(train_loader), imgs)
                 except ZeroTensorPrediction:
                     break
-                # else: 
-                #     print_intermadiate_results(i, X, X_ind, Xreco, name, epoch, loss, len(train_loader), imgs)
-        s = '%d: Train time:%.2f min in %d steps'%(epoch, (time() - a)/60, len(train_loader))
-        print(s)
+        print('%d: Train time:%.2f min in %d steps'%(epoch, (time() - a)/60, len(train_loader)))
         resnet.eval()
-        print(">> Validation: Good samples <<<<<<<<")
+        print(">>>>>>> Validating")
         _eval, emd_vals, ind_loss = do_eval(resnet, val_loader, epoch, X_ind, n_channels)
-        if e == 0: 
-            new_model= True
-            print ('new model')
-        else: 
-            new_model= False
-        save_model(wdir, expt_name, epoch, resnet, optimizer, _eval, emd_vals, ind_loss, new_model, name)
-        #metrics.append(_eval)
-
+        new_model = [True if e == 0 else False]
+        save_model(wdir, expt_name, epoch, resnet, optimizer, _eval, emd_vals, ind_loss, new_model[0], name)
